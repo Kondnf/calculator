75a16f125a9d7b51ba6ecd3f76ef756821b18e14
гк <@adamant.local>
2022-02-10 18:36:07 +0300

    calc1.5

---
+++ zadacha3.js
@@ -0,0 +1,15 @@
+function numbers(arr) {
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    let sum = 0;
+    for(let i = min; i < max; i++){
+        if(!arr.includes(i)){
+        sum=sum+i;
+        }
+    }
+    return sum;
+    }
+      console.log(numbers([17,16,15,10,11,12]))
+
+
+    //   создайте функцию, которая возвращает сумму пропавших чисел из массива которую можно задать
